7e0eda70e8f28a6e07800644788b4e5aaa851007
feibowei <1906524955@qq.com>
2022-01-20 13:48:45 +0800

    fix: prettier config.ts

--- config.ts
+++ config.ts
@@ -1,11 +1,11 @@
 export const copywriter = {
-  global:{
-    header:'1-SYNC'
+  global: {
+    header: '1-SYNC'
   },
-  login:{},
-  register:{},
-  application:{},
-  collection:{},
-  synchronization:{},
-  adminWallet:{}
+  login: {},
+  register: {},
+  application: {},
+  collection: {},
+  synchronization: {},
+  adminWallet: {}
 }
